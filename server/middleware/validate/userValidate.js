6d99ad1657423289d86e9dd6389a3001002b6a4f
sillywhale <ng.nghinhan.dev@gmail.com>
2024-01-08 15:13:25 +0700

    -create validate, add user signUp validate

---
+++ server/middleware/validate/userValidate.js
@@ -0,0 +1,16 @@
+const { validator } = require("./main");
+const { body } = require("express-validator");
+
+const signUpValidator = () =>
+  validator([
+    body("userName").notEmpty().withMessage("Username is required"),
+    body("email").isEmail().withMessage("Invalid email format"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long"),
+    body("confirmPassword")
+      .custom((value, { req }) => value === req.body.password)
+      .withMessage("Passwords do not match"),
+  ]);
+
+module.exports = { signUpValidator };
