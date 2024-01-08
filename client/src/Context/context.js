68e1ec86b2f21cced19743ef6595b5fb691c1985
sillywhale <ng.nghinhan.dev@gmail.com>
2024-01-08 16:40:39 +0700

    -edit userContext

--- client/src/Context/context.js
+++ client/src/Context/context.js
@@ -1,3 +1,7 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
-export const UserArr = createContext();
+export const User = createContext();
+
+export function useUserContext() {
+  return useContext(User);
+}
--- client/src/Context/provider.jsx
+++ client/src/Context/provider.jsx
@@ -1,37 +1,13 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
-import { UserArr } from "./context"; // user array context
-import { useDispatch } from "react-redux";
-import { cartAction } from "../store/store";
+import { useState } from "react";
+import { User } from "./context";
+// import { useDispatch } from "react-redux";
+// import { cartAction } from "../store/store";
 
 export default function UserProvider({ children }) {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const [userArr, setUserArr] = useState(
-    JSON.parse(localStorage.getItem("userArr")) || []
-  );
+  const [user, setUser] = useState();
 
-  // get the last user didn't log out
-  useEffect(() => {
-    const currentUser = JSON.parse(localStorage.getItem("CURRENT_USER"));
-
-    if (currentUser !== null) {
-      dispatch(cartAction.UPDATE_CART(currentUser.cart));
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // save NEW current_user & user_arr state when cart change
-
-  useEffect(() => {
-    // Save the userArr to localStorage when a new user sign-up
-    localStorage.setItem("userArr", JSON.stringify(userArr));
-  }, [userArr]);
-
-  return (
-    <UserArr.Provider value={{ userArr, setUserArr }}>
-      {children}
-    </UserArr.Provider>
-  );
+  return <User.Provider value={{ user, setUser }}>{children}</User.Provider>;
 }
