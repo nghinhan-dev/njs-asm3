1f6fd802b2a1b4882e754a823a12005e99421cf0
sillywhale <ng.nghinhan.dev@gmail.com>
2024-01-09 14:26:43 +0700

    -add login act, handle state of login/signup

--- client/src/Pages/LoginPage/LoginPage.jsx
+++ client/src/Pages/LoginPage/LoginPage.jsx
@@ -3,19 +3,25 @@
 // import { useDispatch, useSelector } from "react-redux";
 // import { cartAction } from "../../store/store";
 import { useEffect, useState } from "react";
-import { Form, useActionData } from "react-router-dom";
+import { Form, useActionData, useNavigate } from "react-router-dom";
 import { useUserContext } from "../../Context/context";
 
 export default function LoginPage() {
   const [isLogin, setIsLogin] = useState(false);
   const { setUser } = useUserContext();
   const actionData = useActionData();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (actionData.userName) {
-      setUser(() => actionData.userName);
+    if (actionData?.result !== undefined) {
+      setIsLogin((prevState) => !prevState);
     }
-  }, [actionData, setUser]);
+
+    if (actionData?.userName !== undefined) {
+      setUser(() => actionData.userName);
+      navigate("/");
+    }
+  }, [actionData, setIsLogin, setUser, navigate]);
 
   // const dispatch = useDispatch();
   // const cart = useSelector((state) => state.cart);
@@ -32,7 +38,12 @@ export default function LoginPage() {
           <h3>{isLogin ? "Sign In" : "Sign Up"}</h3>
           <Form method="POST" className="h-auto d-flex flex-column">
             <div className="formContainer">
-              <input type="email" id="email" name="email" placeholder="Email" />
+              <input
+                type="text"
+                id="userName"
+                name="userName"
+                placeholder="Username"
+              />
 
               <input
                 type="password"
@@ -46,16 +57,16 @@ export default function LoginPage() {
                 <>
                   <input
                     type="text"
-                    id="userName"
+                    id="confirmPassword"
                     name="confirmPassword"
                     placeholder="Confirm password"
                   />
 
                   <input
                     type="text"
-                    id="userName"
-                    name="userName"
-                    placeholder="User Name"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
                   />
 
                   <input
@@ -71,7 +82,8 @@ export default function LoginPage() {
             <input
               className="bg-dark mb-2 text-white"
               type="submit"
-              value={isLogin ? "Sign In" : "Sign Up"}
+              name="intent"
+              value={isLogin ? "login" : "signup"}
             />
           </Form>
 
