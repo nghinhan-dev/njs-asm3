dad9c6d0498898f999bed8bc9f07a0d2f70ae44a
sillywhale <ng.nghinhan.dev@gmail.com>
2024-01-12 13:01:10 +0700

    update uri

--- client/src/utils/loader.js
+++ client/src/utils/loader.js
@@ -1,7 +1,5 @@
 export async function loader() {
-  const response = await fetch(
-    "https://firebasestorage.googleapis.com/v0/b/funix-subtitle.appspot.com/o/Boutique_products.json?alt=media&token=dc67a5ea-e3e0-479e-9eaf-5e01bcd09c74"
-  );
+  const response = await fetch("http://localhost:5000/");
 
   if (!response.ok) {
     throw new Error("Couldn't fetch data");
