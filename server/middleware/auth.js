5f901e8442d66192e22d4302144bd25c2850dc32
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-01-20 13:23:37 +0700

    store / access userId instead of user detail

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,20 +2,21 @@ const User = require("../models/User");
 
 exports.login = async (req, res, next) => {
   try {
-    if (req.session.user) {
-      const user = await User.findById(req.session.user._id);
+    if (req.session.userID) {
+      const user = await User.findById(req.session.userID);
 
       if (user) {
         req.user = user;
         return next();
       } else {
+        res.status(404).send("User not found");
         throw new Error("User not found");
       }
     } else {
+      res.status(409).send("Login first");
       throw new Error("Login first");
     }
   } catch (error) {
     console.error("Error in loginAuth:", error);
-    res.status(500).send("Internal Server Error");
   }
 };
