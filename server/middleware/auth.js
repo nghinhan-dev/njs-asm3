cd5e1b622fbb3fcb75bcaca4172b0619eb3e64b2
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-01-29 10:51:50 +0700

    fix send invalid JSON

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -9,12 +9,10 @@ exports.login = async (req, res, next) => {
         req.user = user;
         return next();
       } else {
-        res.status(404).send("User not found");
-        throw new Error("User not found");
+        res.status(404).send({ statusText: "User not found" });
       }
     } else {
-      res.status(409).send("Login first");
-      throw new Error("Login first");
+      res.status(409).send({ statusText: "Login first" });
     }
   } catch (error) {
     console.error("Error in loginAuth:", error);
