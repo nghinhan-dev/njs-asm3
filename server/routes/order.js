ea07b463002f845af6cc9da0510214e3c6ba35ed
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-02-01 23:41:49 +0700

    validate order request body

--- server/routes/order.js
+++ server/routes/order.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const orderController = require("../controllers/order");
+const orderValidator = require("../middleware/validate/orderValidate");
 const auth = require("../middleware/auth");
 
-router.post("/post", auth.login, orderController.postOrder);
+router.post(
+  "/post",
+  auth.login,
+  orderValidator.addOrderValidator,
+  orderController.postOrder
+);
 router.get("/", auth.login, orderController.getOrders);
 router.get("/:orderId", auth.login, orderController.getOrder);
 
