c51c90a8aa9098deef0c317387b42f821d31ee2b
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-02-25 20:39:04 +0700

    init Message component

--- admin/src/MainLayout/Navbar/Navbar.jsx
+++ admin/src/MainLayout/Navbar/Navbar.jsx
@@ -13,6 +13,9 @@ export default function Navbar() {
             <a onClick={() => setAuth(null)}>
               <i className="fa-solid fa-arrow-right-from-bracket"></i> Logout
             </a>
+            <Link to={"/message"}>
+              <i className="fa-solid fa-message"></i> Message
+            </Link>
             <p>Database</p>
             <Link to={"/product"}>
               <i className="fa-solid fa-user"></i>Products
